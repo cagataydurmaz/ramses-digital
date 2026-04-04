1a6184daa9c25c1835ffb1e751ebcb425c91ceca
cagataydurmaz <durmazcagatay@gmail.com>
2026-04-04 21:22:49 +0300

    fix: remove unsupported host property from robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,7 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      { userAgent: '*', allow: '/', disallow: ['/api/'] },
-      { userAgent: '*', disallow: '/', host: 'ramsesdigital.vercel.app' },
-    ],
+    rules: { userAgent: '*', allow: '/', disallow: ['/api/'] },
     sitemap: 'https://ramsesdigital.com/sitemap.xml',
   }
 }
